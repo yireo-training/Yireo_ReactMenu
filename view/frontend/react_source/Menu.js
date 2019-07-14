19889da0373a471347ff76e8e075b55b534a9437
Jisse Reitsma <jisse@yireo.com>
2019-07-14 15:20:55 +0200

    Rewrite to functional component

--- view/frontend/react_source/Menu.js
+++ view/frontend/react_source/Menu.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import MenuItem from './MenuItem';
 
-class Menu extends React.Component {
-    render() {
-        if (!this.props.children) {
-            return <React.Fragment>No data found</React.Fragment>;
-        }
-
-        return (
-            <React.Fragment>
-                {this.props.children.map((node) =>
-                    <MenuItem node={node} key={node.id}/>
-                )}
-            </React.Fragment>
-        );
+const Menu = (props) => {
+    if (!props.children) {
+        return <React.Fragment>No data found</React.Fragment>;
     }
-}
+
+    return (
+        <React.Fragment>
+            {props.children.map((node) =>
+                <MenuItem node={node} key={node.id}/>
+            )}
+        </React.Fragment>
+    );
+};
 
 export default Menu;
