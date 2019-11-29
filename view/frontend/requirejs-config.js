1a783247d9f8ef7fefebd3858be1b112caf1832e
Jisse Reitsma <jisse@yireo.com>
2019-11-29 13:39:15 +0100

    Remove obsolete files

--- view/frontend/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            reactMenu: 'Yireo_ReactMenu/js/container',
-            reactMenuComponent: 'Yireo_ReactMenu/js/compiled/Menu'
-        }
-    }
-};
