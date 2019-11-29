052590e97a7b7e6f26ad4680afd1ed7c106f44e6
Jisse Reitsma <jisse@yireo.com>
2019-11-29 16:41:21 +0100

    Fix suffix in URLs

--- view/frontend/react_source/Menu.js
+++ view/frontend/react_source/Menu.js
@@ -3,12 +3,16 @@ import MenuItem from './MenuItem';
 
 const Menu = (props) => {
     const [nodes, setNodes] = useState([]);
+    const [urlSuffix, setUrlSuffix] = useState('');
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const originalContent = {__html: props.originalContent};
 
     const variables = {rootId: 2};
     const query = `query rootCategories($rootId: Int) {
+  storeConfig {
+    category_url_suffix
+  }
   category(id: $rootId) {
     children {
       id
@@ -21,11 +25,11 @@ const Menu = (props) => {
         url: url_path
         name
         level
-        position
       }
     }
   }
 }
+
     `;
 
     useEffect(() => {
@@ -45,26 +49,34 @@ const Menu = (props) => {
                 return response;
             })
             .then(response => response.json())
-            .then(responseJson =>
-                setNodes(responseJson.data.category.children)
+            .then(responseJson => {
+                    setUrlSuffix(responseJson.data.storeConfig.category_url_suffix);
+                    setNodes(responseJson.data.category.children);
+                }
             )
             .catch(error => setError(error));
     }, []);
 
     if (loading) {
-        return <React.Fragment><div dangerouslySetInnerHTML={originalContent}/></React.Fragment>;
+        return <React.Fragment>
+            <div dangerouslySetInnerHTML={originalContent}/>
+        </React.Fragment>;
     }
 
     if (!nodes) {
-        return <React.Fragment><div dangerouslySetInnerHTML={originalContent}/></React.Fragment>;
+        return <React.Fragment>
+            <div dangerouslySetInnerHTML={originalContent}/>
+        </React.Fragment>;
     }
 
     nodes.sort((a, b) => (a.position > b.position) ? 1 : -1);
 
     return (
         <React.Fragment>
-            {nodes.map((node) =>
-                <MenuItem node={node} key={node.id}/>
+            {nodes.map((node) => {
+                    node.url = node.url + urlSuffix;
+                    return <MenuItem node={node} key={node.id}/>;
+                }
             )}
         </React.Fragment>
     );
